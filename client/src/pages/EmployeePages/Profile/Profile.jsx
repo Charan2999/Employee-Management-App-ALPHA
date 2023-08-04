43a9927e506c171f5ef34a2021590066ed668e72
Charan <121006199+Charan2999@users.noreply.github.com>
2023-08-04 21:57:04 +0530

    Update Profile.jsx

--- client/src/pages/EmployeePages/Profile/Profile.jsx
+++ client/src/pages/EmployeePages/Profile/Profile.jsx
@@ -7,7 +7,7 @@ import Loader from "../../../Loader.jsx";
 const getProfileDetails = async () => {
   try {
     const response = await axios.get(
-      `https://server-sx5c.onrender.com/employee/getOne/${auth.currentUser.email}`
+      `https://employee-0131.onrender.com/employee/getOne/${auth.currentUser.email}`
     );
     return response.data.Employee;
   } catch (error) {
